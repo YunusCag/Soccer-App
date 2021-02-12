5a85e7a2aa38c833f216784ab130fb0d6572b006
Yunus Çağlıyan <49796926+YunusCag@users.noreply.github.com>
2021-02-12 16:58:14 +0300

    sport repository created

---
+++ app/src/main/java/com/yunuscagliyan/soccerapp/data/repository/SportRepository.kt
@@ -0,0 +1,27 @@
+package com.yunuscagliyan.soccerapp.data.repository
+
+import com.yunuscagliyan.soccerapp.data.api.SportDataApi
+import com.yunuscagliyan.soccerapp.data.model.Team
+import com.yunuscagliyan.soccerapp.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.lang.Exception
+import javax.inject.Inject
+
+class SportRepository @Inject constructor(
+    private val api: SportDataApi
+) {
+    
+    suspend fun getLeagueTeams(
+        countryId: Int = 48,
+        leagueId: Int = 314
+    ): Flow<Resource<List<Team?>>> = flow {
+        emit(Resource.loading(null))
+        try {
+            val response = api.getLeagueTeams(countryId, leagueId)
+            emit(Resource.success(response.teams))
+        } catch (e: Exception) {
+            emit(Resource.error(e.message, null))
+        }
+    }
+}
