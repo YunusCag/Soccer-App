24959dfa29a4359fa9887912cbecf854415e77e8
Yunus Çağlıyan <49796926+YunusCag@users.noreply.github.com>
2021-02-14 15:43:09 +0300

    fake football api deployed on heroku

--- app/src/main/java/com/yunuscagliyan/soccerapp/constants/AppConstant.kt
+++ app/src/main/java/com/yunuscagliyan/soccerapp/constants/AppConstant.kt
@@ -3,5 +3,5 @@ package com.yunuscagliyan.soccerapp.constants
 object AppConstant {
     const val API_KEY = "e578cfd0-6d34-11eb-8cda-0b81196afa50"
     //const val BASE_URL="https://app.sportdataapi.com/api/v1/"
-    const val BASE_URL="http://192.168.43.86:5000/api/"
+    const val BASE_URL="https://soccer-app-backend.herokuapp.com/api/"
 }
